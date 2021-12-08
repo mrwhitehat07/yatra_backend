3bdc4cf07ed961f312e3fbed1a80d4ca5dfd21e3
mrwhitehat07 <bishalmagr70@gmail.com>
2021-12-08 12:36:09 +0545

    initial starter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+});
+
+app.listen(port, () => {
+    console.log("listening at port ",port)
+})
