a5f914e40c3c13d74c5586050b7033b76df4463c
mrwhitehat07 <bishalmagr70@gmail.com>
2021-12-19 12:09:05 +0545

    validation added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const User = require("../models/userModel");
-const Token = require("../models/tokenModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
@@ -9,28 +8,45 @@ const salt = process.env.SALT;
 const secretKey = process.env.SECRET_KEY;
 const transporter = require("../config/emailer.config");
 const { saveToken } = require("../controllers/tokenController");
+const validator = require("validator");
+
+const validateEmail = (email) => {
+    const isEmail = validator.isEmail(email);
+    return isEmail;
+}
+
+const sendMail = async (email) => {
+    await transporter.sendMail({
+        to: email, 
+        subject: "Verify email address",
+        html: "This is your code: "+7976
+    });
+}
 
 router.post('/register', parser, async (req, res) => {
+    const email = req.body.email;
+    const password = await bcrypt.hash(req.body.password, parseInt(salt));
     const newUser = new User({
-        email: req.body.email,
-        password: await bcrypt.hash(req.body.password, parseInt(salt))
+        email: email,
+        password: password
     });  
     try {
-        const user = await User.findOne({email: req.body.email});
-        if(!user) {
-            await newUser.save();
-            await transporter.sendMail({
-                to: req.body.email, 
-                subject: "Verify account",
-                html: "This is your code: "+7976
-            })
-            res.status(200).send({
-                message: "email sent for verification, please check & verify",
-                data: newUser
-            });
+        if(validateEmail(email) == true){
+            const user = await User.findOne({email: email});
+            if(!user) {
+                await newUser.save();
+                res.status(200).send({
+                    message: "email sent for verification, please check & verify",
+                    data: newUser
+                });
+            }else{
+                res.status(403).send({
+                    message: "Email already in use.",
+                });
+            }
         } else {
-            res.status(400).send({
-                message: "Email already in use."
+            res.status(403).send({
+                message: "Please enter valid email"
             });
         }        
     } catch (err) {
@@ -39,29 +55,34 @@ router.post('/register', parser, async (req, res) => {
 });
 
 router.post("/login", parser, async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
     try {
-        const user = await User.findOne({ email: req.body.email });
-        !user && res.status(400).send({
-            message: "Not found!"
-        });
-    
-        const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).send({
-            message: "Wrong credentials!"
-        });
+        if(validateEmail(email) == true){
+            const user = await User.findOne({ email: email });
+            !user && res.status(400).send({
+                message: "Not found!"
+            });
         
-        const access_token = jwt.sign({uuid: user._id}, secretKey, {expiresIn: "24hr"});
-        
-        await saveToken(user._id, access_token);
+            const validated = await bcrypt.compare(password, user.password);
+            !validated && res.status(400).send({
+                message: "Wrong credentials!"
+            });
+            
+            const access_token = jwt.sign({uuid: user._id}, secretKey, {expiresIn: process.env.TOKEN_EXPIRE});
+            
+            await saveToken(user._id, access_token);
 
-        res.status(200).send({
-            token: access_token, 
-            message: "success", 
-            isVerified: user.isVerified
-        });
-     
+            res.status(200).send({
+                token: access_token, 
+                message: "success", 
+            });
+        } else {
+            res.status(403).send({
+                message: "Please enter valid email"
+            });
+        }  
     } catch (err) {
-        console.log(err);
         res.status(500).send(err);
     }
 });
